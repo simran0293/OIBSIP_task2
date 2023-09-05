51c52c5b87cfae86a7cff2c335fb473d02f20ccc
simran0293 <91543406+simran0293@users.noreply.github.com>
2023-09-05 12:09:02 +0530

    Update AtmOperationImpl.java

--- AtmMachine/AtmOperationImpl.java
+++ AtmMachine/AtmOperationImpl.java
@@ -1,3 +1,4 @@
+//This is the seperate file of interface implementation code for your help
 
 public class AtmOperationImpl implements AtmOperationinterf {
     Atm atm=new Atm();
@@ -8,16 +9,30 @@ public class AtmOperationImpl implements AtmOperationinterf {
 
     public void withdrawAmount(double withdrawAmount)
     {
-
+    if (withdrawAmount % 500 == 0) {
+            if (withdrawAmount <= atm.getBalance()) {
+                ministat.put(withdrawAmount, "Amount withdraw");
+                System.out.println("Collect the cash " + withdrawAmount);
+                atm.setBalance(atm.getBalance() - withdrawAmount);
+                viewBalance();
+            } else {
+                System.out.println("Insufficient Balance!!");
+            }
+        } else {
+            System.out.println("Please enter the amount in multiple of 500");
+        }
     }
     public void depositAmount(double depositAmount)
     {
-        System.out.println(depositAmount+" Deposited Successfully");
-        atm.setBalance(atm.getBalance()+ depositAmount);
+        ministat.put(depositAmount, "Amount Deposited");
+        System.out.println(depositAmount + " Deposited Successfully");
+        atm.setBalance(atm.getBalance() + depositAmount);
         viewBalance();
     }
     public void viewMiniStatement()
     {
-
+        for (Map.Entry<Double, String> m : ministat.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
     }
 }
